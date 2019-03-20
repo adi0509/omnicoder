fba1bed5ad22f8961a74c458fadf04e61c343b50
Faisal Akhtar <41644472+faisalAkhtar@users.noreply.github.com>
2019-03-20 10:37:09 +0530

    Add CSS

--- omnicoder2/submitted.php
+++ omnicoder2/submitted.php
@@ -2,9 +2,18 @@
 <html>
 <head>
 	<title>MCQ Round</title>
+	<link rel="stylesheet" type="text/css" href="../style.css">
 </head>
 <body>
-		<h1>You had successfully submitted the form.</h1>
+	<header>
+		<img src="../favicon.png" style="height:100%">
+		<h1>Turington 2k19</h1>
+	</header>
+
+<div class="container">
+		<h1>Your answers were submitted successfully.</h1>
 		<h3>Please wait for the result.</h3>
+</div>
+<footer>©Turington 2019</footer>
 </body>
 </html>
